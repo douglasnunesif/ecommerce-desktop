6857021bcb82c6e5c664adca68dd9fda5befb391
Douglas Nunes <douglas.nunes@ifsuldeminas.edu.br>
2026-04-09 17:23:34 -0300

    Tela Login

--- src/main/java/br/edu/tds/telalogin/TelaCadastroUsuarioController.java
+++ src/main/java/br/edu/tds/telalogin/TelaCadastroUsuarioController.java
@@ -29,43 +29,54 @@ public class TelaCadastroUsuarioController implements Initializable {
     private TextField txtEmail;
     @FXML
     private TextField txtCPF;
-        
-        
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    } 
-    
-     @FXML
+    }
+
+    @FXML
     private void abrirTelaLogin() throws IOException {
         App.setRoot("telaLogin");
     }
-    
-    
+
     @FXML
-    private void cadastrarUsuario(){
-        
+    private void cadastrarUsuario() {
+
         String nomeCompleto = txtNomeCompleto.getText();
         String nomeUsuario = txtNomeUsuario.getText();
         String senha = txtSenha.getText();
         String email = txtEmail.getText();
         String cpf = txtCPF.getText();
-        
-        if(nomeCompleto.isBlank() && nomeUsuario.isEmpty() && senha.isEmpty() && email.isEmpty() && cpf.isEmpty()){
-            System.out.println("Os campos do formulário são obrigatórios");
-            return;
-        }
-        
-        if(nomeCompleto.isEmpty()){
+
+        txtNomeCompleto.setStyle("-fx-background-color: transparent; -fx-border-color: #0598ff; -fx-border-width: 0 0 3 0;");
+        txtNomeUsuario.setStyle("-fx-background-color: transparent; -fx-border-color: #0598ff; -fx-border-width: 0 0 3 0;");
+        txtSenha.setStyle("-fx-background-color: transparent; -fx-border-color: #0598ff; -fx-border-width: 0 0 3 0;");
+        txtEmail.setStyle("-fx-background-color: transparent; -fx-border-color: #0598ff; -fx-border-width: 0 0 3 0;");
+        txtCPF.setStyle("-fx-background-color: transparent; -fx-border-color: #0598ff; -fx-border-width: 0 0 3 0;");
+
+        if (nomeCompleto.isEmpty()) {
             txtNomeCompleto.setStyle("-fx-background-color: transparent; -fx-border-color: red; -fx-border-width: 0 0 3 0;");
-            return;
         }
-        
-        //Cadastrando um usuário no BD
-        UsuarioDAO dao = new UsuarioDAO();
-        Usuario u = new Usuario(nomeCompleto, nomeUsuario, email,senha,cpf);
-        dao.cadastrar(u);       
-        
+        if (nomeUsuario.isEmpty()) {
+            txtNomeUsuario.setStyle("-fx-background-color: transparent; -fx-border-color: red; -fx-border-width: 0 0 3 0;");
+        }
+        if (senha.isEmpty()) {
+            txtSenha.setStyle("-fx-background-color: transparent; -fx-border-color: red; -fx-border-width: 0 0 3 0;");
+        }
+        if (email.isEmpty()) {
+            txtEmail.setStyle("-fx-background-color: transparent; -fx-border-color: red; -fx-border-width: 0 0 3 0;");
+        }
+        if (cpf.isEmpty()) {
+            txtCPF.setStyle("-fx-background-color: transparent; -fx-border-color: red; -fx-border-width: 0 0 3 0;");
+        }
+
+        if (!(nomeCompleto.isEmpty() || nomeUsuario.isEmpty() || senha.isEmpty() || email.isEmpty() || cpf.isEmpty())) {
+            //Cadastrando um usuário no BD
+            UsuarioDAO dao = new UsuarioDAO();
+            Usuario u = new Usuario(nomeCompleto, nomeUsuario, email, senha, cpf);
+            dao.cadastrar(u);
+        }
+
     }
-    
 }
--- src/main/java/br/edu/tds/telalogin/TelaLoginController.java
+++ src/main/java/br/edu/tds/telalogin/TelaLoginController.java
@@ -7,56 +7,71 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
 public class TelaLoginController {
-    
+
     @FXML
     private TextField txtUsuario;
-    @FXML 
+    @FXML
     private PasswordField txtSenha;
     @FXML
     private Label lblUsuario;
     @FXML
     private Label lblSenha;
-    
-    
-    
-       
+
     @FXML
     private void abrirTelaCadastroUsuario() throws IOException {
         App.setRoot("telaCadastroUsuario");
     }
-    
+
     @FXML
-    private void realizarLogin(){
-       
+    private void realizarLogin() {
+
         String usuario = txtUsuario.getText();
         String senha = txtSenha.getText();
-        
-        if(usuario.isEmpty() && senha.isEmpty()){
+
+        if (usuario.isEmpty() && senha.isEmpty()) {
             lblUsuario.setText("* Campo usuário é obrigatório");
             lblSenha.setText("* Campo senha é obrigatório");
             System.out.println("Campo usuário e campo senha são obrigatórios");
-            return; 
+            return;
         }
-        
-        if(usuario.isEmpty()){
+
+        if (usuario.isEmpty()) {
             lblUsuario.setText("* Campo usuário é obrigatório");
             lblSenha.setText("");
             System.out.println("Campo usuário é obrigatório");
             return;
         }
-        if(senha.isEmpty()){
+        if (senha.isEmpty()) {
             lblUsuario.setText("");
             lblSenha.setText("* Campo senha é obrigatório");
             System.out.println("Campo senha é obrigatório");
             return;
         }
-        
+
         lblUsuario.setText("");
         lblSenha.setText("");
+
+        UsuarioDAO dao = new UsuarioDAO();
+        Boolean login = dao.login(usuario, senha);
+        System.out.println(dao.login(usuario, senha));
+
+        if (login) {
+            //Login com sucesso
+            txtUsuario.setStyle("-fx-background-color: transparent; -fx-border-color: green; -fx-border-width: 0 0 3 0;");
+            txtSenha.setStyle("-fx-background-color: transparent; -fx-border-color: green; -fx-border-width: 0 0 3 0;");
+            lblUsuario.setText("");
+            lblSenha.setText("");
+            System.out.println("Login feito");
+        } else {
+            //Falha no login (usuário ou senha inválido)
+            lblUsuario.setText("Usuário/Senha incorreto(a)");
+            lblSenha.setText("Usuário/Senha incorreto(a)");
+            System.out.println("Falha no login");
+        }
+
         System.out.println("Usuário: " + usuario);
         System.out.println("Senha  : " + senha);
-        
-        
+
     }
-    
+
 }
