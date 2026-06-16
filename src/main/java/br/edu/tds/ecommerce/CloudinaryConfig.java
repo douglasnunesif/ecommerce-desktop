0221568de5be7ea08e16ef27eac887aaab7fa01b
Douglas Fabiano de Sousa Nunes <144865713+douglasnunesif@users.noreply.github.com>
2026-06-15 22:19:07 -0300

    Clear Cloudinary configuration
    Removed sensitive Cloudinary configuration values.

--- src/main/java/br/edu/tds/ecommerce/CloudinaryConfig.java
+++ src/main/java/br/edu/tds/ecommerce/CloudinaryConfig.java
@@ -16,9 +16,9 @@ public class CloudinaryConfig {
             Map<String, String> config =
                     new HashMap<>();
 
-            config.put("cloud_name", "ddcpqtkae");
-            config.put("api_key", "435159391975375");
-            config.put("api_secret", "e5qEkMe0AdKs2bH8xfD2v5a7lCQ");
+            config.put("cloud_name", "");
+            config.put("api_key", "");
+            config.put("api_secret", "");
 
             cloudinary =
                     new Cloudinary(config);
